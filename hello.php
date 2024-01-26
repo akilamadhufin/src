bf195c0d1ce44f99e2fc2e8d591b82fdeca03dbb
akilamadhufin <akila.randunupathirannehelage@student.hamk.fi>
2024-01-26 10:58:37 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- html comment-->
+
+<?php
+
+// printing hello world
+
+# this is also single line comment
+
+/* mulitple 
+line code */
+
+echo "<h1>Hello World</h1>";
+?>
+
+    
+</body>
+</html>
+
